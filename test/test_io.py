ee93f9d600cc4fe0e1ba8e58fa689ff5d5ff9727
nisikin <nisikin_1125@163.com>
2025-05-14 17:39:58 +0800

    图像加载与保存

---
+++ test/test_io.py
@@ -0,0 +1,21 @@
+from src.basic.io import load_image, save_image
+
+def test_io():
+    try:
+        # 加载图像
+        print(f"正在加载图像...")
+        img = load_image("../assets/test.jpg")
+
+        # 保存图像为 BMP 格式
+        print(f"正在保存图像...")
+        save_image(img, "../assets/test.bmp")
+
+        print(f"图像加载并保存成功...")
+
+    except FileNotFoundError as e:
+        print(f"文件未找到")
+    except Exception as e:
+        print(f"发生错误")
+
+if __name__ == "__main__":
+    test_io()
