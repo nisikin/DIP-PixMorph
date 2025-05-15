54173b916eb0122e8acce5b3e475f074da7cccfd
nisikin <nisikin_1125@163.com>
2025-05-15 09:05:34 +0800

    update grayscale_image

--- src/basic/grayscale.py
+++ src/basic/grayscale.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
 
-def to_grayscale(image: np.ndarray) -> np.ndarray:
+def grayscale_image(image: np.ndarray) -> np.ndarray:
     """将图像转换为灰度图"""
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
--- test/test_grayscale.py
+++ test/test_grayscale.py
@@ -1,13 +1,13 @@
 import os
 from src.basic.io import load_image, save_image
-from src.basic.grayscale import to_grayscale
+from src.basic.grayscale import grayscale_image
 
 def test_to_grayscale():
     input_path = os.path.join("../assets", "test.jpg")
     output_path = os.path.join("../assets", "test_gray.jpg")
 
     img = load_image(input_path)
-    gray = to_grayscale(img)
+    gray = grayscale_image(img)
     save_image(gray, output_path)
     print("灰度化测试完成，输出路径：", output_path)
 
