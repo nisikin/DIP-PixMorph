a1cf5f037cf0c9d23c5589f5ddb54aa64b4a3b9e
nisikin <nisikin_1125@163.com>
2025-06-15 17:28:23 +0800

    训练脚本

--- src/style_transfer/scripts/train.py
+++ src/style_transfer/scripts/train.py
@@ -3,7 +3,7 @@ import subprocess
 def run_neural_style_train(dataset, style_image, save_model_dir,
                            epochs=10, batch_size=4, image_size=256, accel=True):
     cmd = [
-        "python", "neural_style.py", "train",
+        "python", "../neural_style/neural_style.py", "train",
         "--dataset", dataset,
         "--style-image", style_image,
         "--save-model-dir", save_model_dir,
